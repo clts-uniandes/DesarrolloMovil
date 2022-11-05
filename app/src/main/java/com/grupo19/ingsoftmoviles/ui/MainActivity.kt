c6579b5f3e2e24bcad387870ec3771a3b8ab11d8
Luis Solier Sajami <luis.sajami@gmail.com>
2022-11-05 18:21:34 -0500

    Add layouts and viewModel for album list

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.grupo19.ingsoftmoviles.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.grupo19.ingsoftmoviles.R
+import com.grupo19.ingsoftmoviles.databinding.ActivityMainBinding
+import com.grupo19.ingsoftmoviles.ui.adapters.AlbumAdapter
+import com.grupo19.ingsoftmoviles.viewmodel.AlbumViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val albumViewModel by viewModels<AlbumViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        albumViewModel.albums.observe(this) {
+                albums -> binding.albumsRecyclerView.adapter = AlbumAdapter(this, albums)
+        }
+
+        albumViewModel.progressVisible.observe(this) {
+            binding.progress.visibility = if (it) View.VISIBLE else View.GONE
+        }
+
+        albumViewModel.showMessage.observe(this) {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        }
+
+        albumViewModel.onCreate()
+    }
+}
---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/adapters/AlbumAdapter.kt
@@ -0,0 +1,46 @@
+package com.grupo19.ingsoftmoviles.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.grupo19.ingsoftmoviles.R
+import com.grupo19.ingsoftmoviles.model.data.Album
+
+class AlbumAdapter(private val context: Context, private val albumsList: List<Album>): RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
+
+    /**
+     * Create new views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.album_item, parent, false)
+        return AlbumViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        val album = albumsList[position]
+        holder.albumNameTextView.text = album.name
+        holder.performersNameTextView.text = album.performers
+//        Glide.with(context)
+//            .load(album.cover)
+//            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return albumsList.size
+    }
+
+    class AlbumViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+        val albumNameTextView: TextView = view.findViewById(R.id.album_name)
+        val performersNameTextView: TextView = view.findViewById(R.id.performers)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+    }
+}
