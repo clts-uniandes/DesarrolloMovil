2233b705be056a3cf4b2298a9289ae6fb04f3b71
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-11-06 10:16:31 -0500

    construccion de layout

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/AsociateTrackAlbumActivity.kt
@@ -0,0 +1,12 @@
+package com.grupo19.ingsoftmoviles.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupo19.ingsoftmoviles.R
+
+class AsociateTrackAlbumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asociate_track_album)
+    }
+}
