2ad726022dc63b04671b8d08a3198ef65ec4a03c
Luis Solier Sajami <luis.sajami@gmail.com>
2022-11-06 10:08:20 -0500

    Add unit test

--- app/src/main/java/com/grupo19/ingsoftmoviles/model/network/album/AlbumServiceAdapterImpl.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/model/network/album/AlbumServiceAdapterImpl.kt
@@ -1,16 +1,14 @@
 package com.grupo19.ingsoftmoviles.model.network.album
 
 import android.util.Log
-import com.grupo19.ingsoftmoviles.model.data.Album
 import com.grupo19.ingsoftmoviles.model.data.AlbumResponse
 
-class AlbumServiceAdapterImpl: AlbumServiceAdapter {
+class AlbumServiceAdapterImpl(private val clientBuilder: RetrofitBuilder = RetrofitBuilder()): AlbumServiceAdapter {
 
     override suspend fun getAlbums(): List<AlbumResponse> {
 
-        Log.i("AlbumServiceAdapterImpl", "Getting albums from back-vinyls")
-
-        return AlbumClient.service.getAlbums()
+        //Log.i("AlbumServiceAdapterImpl", "Getting albums from back-vinyls")
+        return clientBuilder.createClient(AlbumServiceRetrofit::class.java).getAlbums()
     }
 
 }
