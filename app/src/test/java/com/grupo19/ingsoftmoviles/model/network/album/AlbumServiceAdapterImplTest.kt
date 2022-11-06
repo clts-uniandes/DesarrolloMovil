d8dc1275a02161aa75c56b74e5d9e65b97a175a6
Luis Solier Sajami <luis.sajami@gmail.com>
2022-11-06 10:08:20 -0500

    Add unit test

---
+++ app/src/test/java/com/grupo19/ingsoftmoviles/model/network/album/AlbumServiceAdapterImplTest.kt
@@ -0,0 +1,27 @@
+package com.grupo19.ingsoftmoviles.model.network.album
+
+import com.google.gson.Gson
+import com.grupo19.ingsoftmoviles.model.data.AlbumResponse
+import com.grupo19.ingsoftmoviles.util.FileUtils
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class AlbumServiceAdapterImplTest {
+
+    @Test
+    fun get_albums_is_ok()  = runBlocking {
+        val retrofitBuilderMock : RetrofitBuilder = mock(RetrofitBuilder::class.java)
+        val albumServiceMock: AlbumServiceRetrofit = mock(AlbumServiceRetrofit::class.java)
+        val albumServiceAdapter: AlbumServiceAdapter = AlbumServiceAdapterImpl(retrofitBuilderMock)
+        val albumResponseList: List<AlbumResponse> = listOf(Gson().fromJson(FileUtils.readFile("json/album_response.json"), AlbumResponse::class.java))
+
+        `when`(retrofitBuilderMock.createClient(AlbumServiceRetrofit::class.java)).thenReturn(albumServiceMock)
+        `when`(albumServiceMock.getAlbums()).thenReturn(albumResponseList)
+
+        val albums = albumServiceAdapter.getAlbums()
+
+        Assert.assertEquals(albums[0].name, "Buscando América")
+    }
+}
