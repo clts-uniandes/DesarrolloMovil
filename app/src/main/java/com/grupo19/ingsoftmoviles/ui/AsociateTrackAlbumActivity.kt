2bc0eaf589f1f36b8ade700d7d2fa28a5abac2ff
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-11-06 18:05:29 -0500

    Creacion de modelview para creacion de tracks

--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/AsociateTrackAlbumActivity.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/AsociateTrackAlbumActivity.kt
@@ -3,6 +3,7 @@ package com.grupo19.ingsoftmoviles.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import com.grupo19.ingsoftmoviles.R
@@ -10,36 +11,37 @@ import com.grupo19.ingsoftmoviles.databinding.ActivityAsociateTrackAlbumBinding
 import com.grupo19.ingsoftmoviles.databinding.ActivityMainBinding
 import com.grupo19.ingsoftmoviles.model.data.TrackCreate
 import com.grupo19.ingsoftmoviles.model.repo.TrackRepository
+import com.grupo19.ingsoftmoviles.viewmodel.TrackViewModel
 import kotlinx.coroutines.launch
 
 class AsociateTrackAlbumActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAsociateTrackAlbumBinding
 
+    private val trackViewModel by viewModels<TrackViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_asociate_track_album)
+        setContentView(R.layout.activity_asociate_track_album)
 
         binding = ActivityAsociateTrackAlbumBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.buttonAsociate.setOnClickListener{
             createTrack()
+        }
+        trackViewModel.statusMessage.observe(this){
+            if(it){
+                Toast.makeText(this, "Track Creado", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Ha ocurrido un error, intente nuevamente", Toast.LENGTH_LONG).show()
+            }
+
+
         }
     }
     fun createTrack(){
         val repository : TrackRepository = TrackRepository()
         val trackCreate : TrackCreate = TrackCreate("test1", "4:00")
-
-        lifecycleScope.launch {
-            val response = repository.createTrack(11, trackCreate)
-            if (response){
-                println("funciona")
-            }else {
-                println("no funciona")
-            }
-        }
-
-
+        trackViewModel.onCreateTrack(11, trackCreate)
     }
 }
---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/viewmodel/TrackViewModel.kt
@@ -0,0 +1,23 @@
+package com.grupo19.ingsoftmoviles.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.grupo19.ingsoftmoviles.model.data.TrackCreate
+import com.grupo19.ingsoftmoviles.model.repo.TrackRepository
+import kotlinx.coroutines.launch
+
+class TrackViewModel(private val trackRepository: TrackRepository = TrackRepository()): ViewModel() {
+    private val _statusMessage = MutableLiveData<Boolean>()
+
+    val statusMessage: LiveData<Boolean> get() = _statusMessage
+
+    fun onCreateTrack(id_album:Int, trackCreate: TrackCreate){
+        val id = id_album
+        val track = trackCreate
+        viewModelScope.launch {
+            _statusMessage.value = trackRepository.createTrack(id, track)
+        }
+    }
+}
