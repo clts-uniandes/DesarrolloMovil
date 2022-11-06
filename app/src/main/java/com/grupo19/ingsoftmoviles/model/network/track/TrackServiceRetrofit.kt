9a1152f60148f997e0b58cd9ccc1537272600307
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-11-06 11:09:52 -0500

    Repository, ServiceAdapter creacion

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/model/network/track/TrackServiceRetrofit.kt
@@ -0,0 +1,12 @@
+package com.grupo19.ingsoftmoviles.model.network.track
+
+import com.grupo19.ingsoftmoviles.model.data.TrackCreate
+import com.grupo19.ingsoftmoviles.model.data.TrackResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface TrackServiceRetrofit{
+    @POST("/albums/{id_album}/tracks")
+    fun creteTrack(@Path("id_album")  id_album:Int, @Body trackCreate: TrackCreate):TrackResponse
+}
