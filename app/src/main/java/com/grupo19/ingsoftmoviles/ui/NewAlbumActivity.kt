07a1ed0cd2beb4a30b4926320ccdf8257deaae30
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-11-06 20:36:26 -0500

    feat: migracion a material design

--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/NewAlbumActivity.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/NewAlbumActivity.kt
@@ -2,15 +2,10 @@ package com.grupo19.ingsoftmoviles.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.grupo19.ingsoftmoviles.databinding.NewAlbumItemBinding
+import com.grupo19.ingsoftmoviles.databinding.ActivityNewAlbumBinding
 
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.viewModels
 import com.grupo19.ingsoftmoviles.R
 import com.grupo19.ingsoftmoviles.model.ResultWrapper
@@ -18,7 +13,7 @@ import com.grupo19.ingsoftmoviles.viewmodel.NewAlbumViewModel
 
 class NewAlbumActivity : AppCompatActivity() {
 
-    private lateinit var binding: NewAlbumItemBinding
+    private lateinit var binding: ActivityNewAlbumBinding
     private val newAlbumViewModel by viewModels<NewAlbumViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,11 +25,11 @@ class NewAlbumActivity : AppCompatActivity() {
         var selectedRecordLabel : String = recordLabels[0]
 
         super.onCreate(savedInstanceState)
-        binding = NewAlbumItemBinding.inflate(layoutInflater)
+        binding = ActivityNewAlbumBinding.inflate(layoutInflater)
         //setContentView(R.layout.new_album_item)
         setContentView(binding.root)
 
-        val genreAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genres)
+        /*val genreAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genres)
         genreAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         val genreSpinner = findViewById<Spinner>(R.id.spinnerGenre)
         genreSpinner.adapter = genreAdapter
@@ -44,9 +39,9 @@ class NewAlbumActivity : AppCompatActivity() {
                 selectedGenre = genres[position]
             }
             override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
+        }*/
 
-        val recordLabelAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, recordLabels)
+        /*val recordLabelAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, recordLabels)
         recordLabelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         val recordLabelSpinner = findViewById<Spinner>(R.id.spinnerRecordLabel)
         recordLabelSpinner.adapter = recordLabelAdapter
@@ -56,6 +51,26 @@ class NewAlbumActivity : AppCompatActivity() {
                 selectedRecordLabel = recordLabels[position]
             }
             override fun onNothingSelected(parent: AdapterView<*>?) { }
+        }*/
+
+        val genreDropdownAdapter = ArrayAdapter(this,R.layout.material_list_item, genres)
+        val genreDropdown = findViewById<AutoCompleteTextView>(R.id.spinnerGenre)
+        genreDropdown.setAdapter(genreDropdownAdapter)
+        genreDropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                selectedGenre = genres[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        val labelDropdownAdapter = ArrayAdapter(this,R.layout.material_list_item, genres)
+        val recordLabelDropdown = findViewById<AutoCompleteTextView>(R.id.spinnerRecordLabel)
+        recordLabelDropdown.setAdapter(labelDropdownAdapter)
+        recordLabelDropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                selectedRecordLabel = recordLabels[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
         val createButton: Button = findViewById(R.id.buttonCreateAlbum)
@@ -85,7 +100,7 @@ class NewAlbumActivity : AppCompatActivity() {
 
     }
 
-    fun showToast(msg: String) {
+    private fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 
