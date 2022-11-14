e0479e3f70c3bf0ed70e39a472354cc16562a509
David Zúñiga <d.zunigab@uniandes.edu.co>
2022-11-14 17:37:33 -0500

    Creacion del layout para listado de artistas

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/ArtistsListActivity.kt
@@ -0,0 +1,12 @@
+package com.grupo19.ingsoftmoviles.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupo19.ingsoftmoviles.R
+
+class ArtistsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artists_list)
+    }
+}
