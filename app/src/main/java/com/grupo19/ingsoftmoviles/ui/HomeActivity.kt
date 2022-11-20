1133f7a3ca649063eed4cb981f43946930d7d0fd
Edgar Melara <e.melara@uniandes.edu.co>
2022-11-20 02:05:39 -0600

    feat: agregar barra de navegacion

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.grupo19.ingsoftmoviles.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.grupo19.ingsoftmoviles.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
