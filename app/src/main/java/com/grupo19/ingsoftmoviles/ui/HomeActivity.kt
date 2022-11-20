52ff41171e1edec6ea2dc597d97547d6cf63eb74
Edgar Melara <e.melara@uniandes.edu.co>
2022-11-20 12:26:25 -0600

    feat: agregar navegacion por drawer

--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/HomeActivity.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/HomeActivity.kt
@@ -6,17 +6,23 @@ import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.grupo19.ingsoftmoviles.R
 
 class HomeActivity : AppCompatActivity() {
 
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private var appBarConfiguration: AppBarConfiguration? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_home)
 
         //Initialize the bottom navigation view
@@ -28,6 +34,18 @@ class HomeActivity : AppCompatActivity() {
         //Initialize the drawer navigation view
         //create bottom navigation view object
         drawerLayout = findViewById(R.id.my_drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.albumListFragment,
+                R.id.artistListFragment,
+                R.id.collectionistFragment,
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController!!, appBarConfiguration!!) //the most important part
+        navView.setupWithNavController(navController!!) //the second most important part
+
         actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
 
         // pass the Open and Close toggle for the drawer layout listener
@@ -44,4 +62,6 @@ class HomeActivity : AppCompatActivity() {
             true
         } else super.onOptionsItemSelected(item)
     }
+
+
 }
