15481b131603781be77e13940001e7d691e0f9d7
Edgar Melara <e.melara@uniandes.edu.co>
2022-11-20 10:52:13 -0600

    feat: integrar vista de albumes en navegacion

--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/album/AlbumListFragment.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/album/AlbumListFragment.kt
@@ -1,11 +1,20 @@
 package com.grupo19.ingsoftmoviles.ui.album
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.grupo19.ingsoftmoviles.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.grupo19.ingsoftmoviles.databinding.FragmentAlbumListBinding
+import com.grupo19.ingsoftmoviles.model.data.AlbumResponse
+import com.grupo19.ingsoftmoviles.ui.Constants
+import com.grupo19.ingsoftmoviles.ui.MainActivity2
+import com.grupo19.ingsoftmoviles.ui.adapters.AlbumAdapter
+import com.grupo19.ingsoftmoviles.viewmodel.AlbumViewModel
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +28,28 @@ private const val ARG_PARAM2 = "param2"
  */
 class AlbumListFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentAlbumListBinding? = null
+    private val albumViewModel by viewModels<AlbumViewModel>()
+    private val binding get() = _binding!!
+    private var thiscontext: Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        albumViewModel.albums.observe(this) { albums ->
+            binding.albumsRecyclerView?.adapter = thiscontext?.let { AlbumAdapter(it, albums) { albumViewModel.onAlbumClick(it) } }
         }
+
+        albumViewModel.progressVisible.observe(this) {
+            binding.progress.visibility = if (it) View.VISIBLE else View.GONE
+        }
+
+        albumViewModel.showMessage.observe(this) {
+            showAlbumDetail(it)
+        }
+
+        albumViewModel.onCreate()
+
     }
 
     override fun onCreateView(
@@ -35,8 +57,21 @@ class AlbumListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album_list, container, false)
+        thiscontext = container?.context
+        _binding = FragmentAlbumListBinding.inflate(inflater, container, false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_album_list, container, false)
     }
+
+
+    private fun showAlbumDetail(album: AlbumResponse) {
+        val intent = Intent(thiscontext, MainActivity2::class.java).apply {
+            putExtra(Constants.ALBUM_OBJECT, album)
+        }
+        startActivity(intent)
+    }
+
+
 
     companion object {
         /**
