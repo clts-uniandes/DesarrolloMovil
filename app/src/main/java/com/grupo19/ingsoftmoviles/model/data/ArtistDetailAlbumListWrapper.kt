2f8fba35076153800c43dfe1c05b3709e48ddb61
Luis Solier <luis.sajami@gmail.com>
2022-11-20 19:19:38 -0500

    Add artist detail album list implementation (#111)

---
+++ app/src/main/java/com/grupo19/ingsoftmoviles/model/data/ArtistDetailAlbumListWrapper.kt
@@ -0,0 +1,5 @@
+package com.grupo19.ingsoftmoviles.model.data
+
+class ArtistDetailAlbumListWrapper(
+    val albums: List<ArtistResponse.AlbumResponse>
+): java.io.Serializable
--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/ArtistDetailAlbumListActivity.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/ArtistDetailAlbumListActivity.kt
@@ -1,6 +1,24 @@
 package com.grupo19.ingsoftmoviles.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.grupo19.ingsoftmoviles.databinding.ActivityArtistDetailAlbumListBinding
+import com.grupo19.ingsoftmoviles.model.data.ArtistDetailAlbumListWrapper
+import com.grupo19.ingsoftmoviles.ui.adapters.ArtistDetailAlbumAdapter
 
 class ArtistDetailAlbumListActivity: AppCompatActivity()  {
+
+    private lateinit var binding: ActivityArtistDetailAlbumListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArtistDetailAlbumListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val albumsWrapper: ArtistDetailAlbumListWrapper = intent.getSerializableExtra(Constants.ALBUMS_LIST) as ArtistDetailAlbumListWrapper
+        println(Gson().toJson(albumsWrapper.albums))
+
+        binding.artistDetailAlbumRecyclerView.adapter = ArtistDetailAlbumAdapter(this, albumsWrapper.albums)
+    }
 }
