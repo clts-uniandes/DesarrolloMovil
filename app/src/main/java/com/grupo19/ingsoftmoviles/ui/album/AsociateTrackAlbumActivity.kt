3d54aa42179b774dd979e81605c3c15c443414e0
Luis Solier Sajami <luis.sajami@gmail.com>
2022-11-28 04:01:07 -0500

    Add return navigation after associate a track

--- app/src/main/java/com/grupo19/ingsoftmoviles/ui/album/AsociateTrackAlbumActivity.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/ui/album/AsociateTrackAlbumActivity.kt
@@ -1,9 +1,11 @@
 package com.grupo19.ingsoftmoviles.ui.album
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.LocalContext
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.grupo19.ingsoftmoviles.R
 import com.grupo19.ingsoftmoviles.databinding.ActivityAsociateTrackAlbumBinding
@@ -11,6 +13,7 @@ import com.grupo19.ingsoftmoviles.model.data.TrackCreate
 import com.grupo19.ingsoftmoviles.model.repo.TrackRepository
 import com.grupo19.ingsoftmoviles.ui.Constants
 import com.grupo19.ingsoftmoviles.ui.Constants.ALBUM_ID_ERROR
+import com.grupo19.ingsoftmoviles.ui.HomeActivity
 import com.grupo19.ingsoftmoviles.viewmodel.TrackViewModel
 
 class AsociateTrackAlbumActivity : AppCompatActivity() {
@@ -52,10 +55,10 @@ class AsociateTrackAlbumActivity : AppCompatActivity() {
         }
         trackViewModel.statusMessage.observe(this){
             if(it){
-                showAlert("Track Asociado", "El track ha sido asociado correctamente al album. Ahora puede verlo en el listado.")
+                showAlert("Track Asociado", "El track ha sido asociado correctamente al album. Ahora puede verlo en el listado.", albumId)
                 Toast.makeText(this, "Track Creado", Toast.LENGTH_LONG).show()
             }else{
-                showAlert("Error", "Ocurrio un error asociando el track al album.")
+                showAlert("Error", "Ocurrio un error asociando el track al album.", albumId)
                 Toast.makeText(this, "Ha ocurrido un error, intente nuevamente", Toast.LENGTH_LONG).show()
             }
 
@@ -66,17 +69,22 @@ class AsociateTrackAlbumActivity : AppCompatActivity() {
         val trackCreate : TrackCreate = TrackCreate(name_track, duration_track)
         trackViewModel.onCreateTrack(albumId, trackCreate)
     }
-    fun showAlert(title:String,message:String){
+    fun showAlert(title:String,message:String, albumId:Int){
         val buttonCancel = getString(R.string.button_cancel_pupup_track)
         val buttonOk = getString(R.string.button_ok_pupup_track)
-        MaterialAlertDialogBuilder(this@AsociateTrackAlbumActivity)
+        val context = this@AsociateTrackAlbumActivity
+        MaterialAlertDialogBuilder(context)
             .setTitle(title)
             .setMessage(message)
             .setNegativeButton(buttonCancel) { dialog, which ->
                 // Respond to negative button press
+                val intent = Intent(context, HomeActivity::class.java)
+                context.startActivity(intent)
             }
             .setPositiveButton(buttonOk) { dialog, which ->
                 // Respond to positive button press
+                val intent = Intent(context, HomeActivity::class.java)
+                context.startActivity(intent)
             }
             .show()
     }
