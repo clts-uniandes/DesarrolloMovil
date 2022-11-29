d956465818b6a59a37483ba323024888a6870653
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2022-11-29 00:21:45 -0500

    feat: se habilita cache de Retrofit modo online (modo offline requiere context universal)

--- app/src/main/java/com/grupo19/ingsoftmoviles/model/network/RetrofitBuilder.kt
+++ app/src/main/java/com/grupo19/ingsoftmoviles/model/network/RetrofitBuilder.kt
@@ -1,9 +1,10 @@
 package com.grupo19.ingsoftmoviles.model.network
 
-import okhttp3.OkHttpClient
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitBuilder {
 
@@ -15,10 +16,10 @@ class RetrofitBuilder {
 
         val mOkHttpClient = OkHttpClient
             .Builder()
+            .addNetworkInterceptor(CacheInterceptor())
             .addInterceptor(mHttpLoggingInterceptor)
+            //.addInterceptor(ForceCacheInterceptor())
             .build()
-
-        //var mRetrofit: Retrofit? = null
 
         val retrofit =  Retrofit.Builder()
             .baseUrl(baseUrl)
@@ -27,5 +28,36 @@ class RetrofitBuilder {
             .build()
         return retrofit.create(clazz)
     }
+
+    class CacheInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val response: Response = chain.proceed(chain.request())
+            val cacheControl = CacheControl.Builder()
+                .maxAge(6, TimeUnit.HOURS)
+                .build()
+            return response.newBuilder()
+                .header("Cache-Control", cacheControl.toString())
+                .build()
+        }
+    }
+
+    /*class ForceCacheInterceptor : Interceptor { // if no internet available
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val builder : Request.Builder = chain.request().newBuilder()
+            if (!isInternetAvailable() {
+                builder.cacheControl(CacheControl.FORCE_CACHE);
+            }
+            return chain.proceed(builder.build())
+        }
+
+        private fun isInternetAvailable(context: Context): Boolean {
+            var isConnected: Boolean = false // Initial Value
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+            if (activeNetwork != null && activeNetwork.isConnected)
+                isConnected = true
+            return isConnected
+        }
+    }*/
 
 }
